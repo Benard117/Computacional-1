df411f476982376a9d0ebabb48cc2e8b002b00a0
Benard117 <35703079+Benard117@users.noreply.github.com>
2018-02-23 08:45:22 -0700

    Delete filtro.sh

--- Actividad4/filtro.sh
+++
@@ -1,5 +0,0 @@
-JUNTAR=`cat sounding* > sondeos1.txt`
-echo $JUNTAR
-
-SELECCIONAR=`egrep -v 'PRES|hPa' sondeos.txt | egrep '74560|Showalter|LIFT|SWEAT|K|Totals|CAPE|CINS|LFCT|CAPV|Temp|Pres|thick|Precip' > df2017_2.csv`
-echo $SELECCIONAR 
